b5aaf6d6ddbd246f1c439bad1f2f70d8228baf09
mathias mauraisin <mamaurai@student.42.fr>
2024-05-28 16:03:03 +0200

    .

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ module "credentials" {
   creds_user_path = var.creds_user_path
 }
 
-module "vpc" {
-  source = "./modules/vpc"
-}
 
 provider "aws" {
-   access_key = module.credentials.aws_access_key
-   secret_key = module.credentials.aws_secret_key
-   region = "eu-west-1"
+   # access_key = module.credentials.aws_access_key
+   # access_key = "AKIA3AT2RHOX2SDRSX7G"
+   # secret_key = module.credentials.aws_secret_key
+   # secret_key = "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"
+
+  shared_credentials_files = ["credentials/aws_creds/mamaurai/creds"]
+   region = "eu-west-3"
 }
+module "vpc" { source = "./modules/vpc" }
